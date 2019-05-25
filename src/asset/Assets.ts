fcf42f694daf5498fe0f6cf9908c9fef45ea75c5
Avahe Kellenbeger <avahe@protonmail.ch>
2019-05-24 23:19:12 -0400

    Now using a smaller purple moyo image.

--- src/asset/Assets.ts
+++ src/asset/Assets.ts
@@ -1,7 +1,6 @@
 export class Assets {
-    public static readonly MOYO_BLUE: string = window.location + '/res/moyo_blue.png'
-    public static readonly MOYO_PURPLE: string = window.location + '/res/moyo_purple.png'
+  public static readonly MOYO_BLUE: string = window.location + '/res/moyo_blue.png'
+  public static readonly MOYO_PURPLE: string = window.location + '/res/moyo_purple_small.png'
 
-    public static readonly SOUNDS_PATH: string = window.location + '/res/sounds'
-
+  public static readonly SOUNDS_PATH: string = window.location + '/res/sounds'
 }
