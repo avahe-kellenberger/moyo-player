120a1d45bdd6bb382bb7d0400b7905adc164c123
Avahe Kellenbeger <avahe@protonmail.ch>
2019-05-25 23:40:13 -0400

    Tests now mock css/scss files.

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,49 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const path = require('path')
 
 module.exports = {
-    entry: {
-        index: ['./src/index.tsx', './src/index.scss'],
-    },
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'prod'),
-    },
-    resolve: {
-        modules: ['src', 'node_modules'],
-        extensions: ['*', '.ts', '.tsx', '.js', '.json'],
-    },
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: 'html-loader',
-                    },
-                ],
-            },
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            },
-            {
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                loader: 'awesome-typescript-loader',
-            },
-            {
-                test: /\.(mp3|png|svg|jpg|gif)$/,
-                use: ['file-loader'],
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: './src/index.ejs',
-            filename: 'index.html',
-        }),
-    ],
-};
+  entry: {
+    index: ['./src/index.tsx', './src/index.scss']
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'prod')
+  },
+  resolve: {
+    modules: ['src', 'node_modules'],
+    extensions: ['*', '.ts', '.tsx', '.js', '.json']
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'awesome-typescript-loader'
+      },
+      {
+        test: /\.(mp3|png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.ejs',
+      filename: 'index.html'
+    })
+  ]
+}
