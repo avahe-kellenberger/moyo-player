e1060daebac61211fc4e2129483d1f64a4787d91
Avahe Kellenberger <avahe@protonmail.ch>
2020-02-23 13:35:25 -0500

    Added Gm and F# Yami drums.

--- src/index.tsx
+++ src/index.tsx
@@ -7,14 +7,29 @@ import MoyoLowBMinor from './moyo/configs/MoyoLowBMinor'
 import Pueblo from './moyo/configs/Pueblo'
 import { DrumConfig } from './moyo/DrumConfig'
 import YamiAMinor from './moyo/configs/YamiAMinor'
+import YamiFSharp from './moyo/configs/YamiFSharp'
+import YamiGMinor from './moyo/configs/YamiGMinor'
 
 const root: HTMLElement | null = document.getElementById('root')
 
 const moyoDMinor = new MoyoDMinor()
 const moyoLowBMinor = new MoyoLowBMinor()
 const moyoCMinor = new MoyoCMinor()
-const pueblo = new Pueblo()
-const yamiAMinor = new YamiAMinor()
 
-const configurations: DrumConfig[] = [moyoDMinor, moyoLowBMinor, moyoCMinor, pueblo, yamiAMinor]
+const pueblo = new Pueblo()
+
+const yamiAMinor = new YamiAMinor()
+const yamiFSharp = new YamiFSharp()
+const yamiGMinor = new YamiGMinor()
+
+const configurations: DrumConfig[] = [
+  moyoDMinor,
+  moyoLowBMinor,
+  moyoCMinor,
+  pueblo,
+  yamiAMinor,
+  yamiFSharp,
+  yamiGMinor
+]
+
 ReactDOM.render(<DrumPlayerContainer drumConfigs={configurations} />, root)
---
+++ src/moyo/configs/YamiFSharp.ts
@@ -0,0 +1,73 @@
+import { Assets } from '../../asset/Assets'
+import Polygon from '../../util/Polygon'
+import { DrumConfig } from '../DrumConfig'
+import DrumTongue from '../DrumTongue'
+
+export default class YamiFSharp implements DrumConfig {
+  public readonly scaleName: string = 'Yami F#'
+  public readonly imagePath: string = Assets.RED_YAMI
+  public readonly tongues: DrumTongue[] = [
+    new DrumTongue('3_f_sharp', new Polygon([
+      [255, 374],
+      [205, 304],
+      [202, 276],
+      [215, 246],
+      [249, 228],
+      [277, 228],
+      [297, 241],
+      [346, 313]
+    ])),
+    new DrumTongue('3_a', new Polygon([
+      [47, 303],
+      [101, 243],
+      [120, 235],
+      [144, 238],
+      [173, 264],
+      [184, 283],
+      [185, 302],
+      [180, 315],
+      [134, 374]
+    ])),
+    new DrumTongue('3_b', new Polygon([
+      [381, 208],
+      [313, 222],
+      [285, 213],
+      [268, 190],
+      [266, 165],
+      [279, 138],
+      [296, 126],
+      [359, 117]
+    ])),
+    new DrumTongue('4_c_sharp', new Polygon([
+      [33, 123],
+      [104, 141],
+      [117, 156],
+      [124, 179],
+      [112, 211],
+      [88, 229],
+      [68, 229],
+      [15, 214]
+    ])),
+    new DrumTongue('4_e', new Polygon([
+      [306, 55],
+      [285, 107],
+      [262, 121],
+      [237, 121],
+      [205, 100],
+      [198, 72],
+      [220, 20]
+    ])),
+    new DrumTongue('4_f_sharp', new Polygon([
+      [154, 24],
+      [180, 68],
+      [180, 92],
+      [166, 113],
+      [144, 126],
+      [118, 127],
+      [101, 119],
+      [93, 109],
+      [74, 69]
+    ]))
+  ];
+
+}
